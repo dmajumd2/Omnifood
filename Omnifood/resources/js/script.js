4587c02907b11aec51cd0aea6d603df73bbfdc8c
Debashish <dmajumd2@binghamton.edu>
2018-01-23 13:58:34 -0500

    Add: Added a navigation code for buttons on home page

--- Omnifood/resources/js/script.js
+++ Omnifood/resources/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	
+	/* Sticky navigation */
 	$('.js--section--features').waypoint(function(direction){
 		if(direction == "down"){
 			$('nav').addClass('sticky');
@@ -10,7 +10,31 @@ $(document).ready(function(){
 		offset: '60px'
 		});
 	
+	/* scroll on buttond */
 	
+	$('.js-scroll-to-plans').click(function(){
+		$('html, body').animate({scrollTop: $('.js-section-plans').offset().top}, 1000);
+	});
+	
+	$('.js-scroll-to-start').click(function(){
+		$('html, body').animate({scrollTop: $('.js--section--features').offset().top}, 1000);
+	});
+	
+/*Navigation scroll */	
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
 	/*
 	$('h1').click(function(){
 		$(this).css('background-color', '#ff0000');
